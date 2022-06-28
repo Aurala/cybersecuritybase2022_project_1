76d8b54311c9ad8f658e479ef72d083f7fc34847
Aurala <39862070+Aurala@users.noreply.github.com>
2022-06-28 17:21:09 +0300

    Added the no-login brag view

--- gamez/urls.py
+++ gamez/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('memez/brag/<int:user>/', views.brag, name='brag'),
 ]
--- gamez/views.py
+++ gamez/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+# The main page that lets users manage their game collection
 @login_required
 def index(request):
     return render(request, 'gamez/index.html')
+
+
+# The no-login brag page that a user can share with other people
+def brag(request, user):
+    return render(request, 'gamez/brag.html', {})
