a7d19462f1f553ff0764665b6f29fd6f37c6d4a9
Aurala <39862070+Aurala@users.noreply.github.com>
2022-06-29 22:37:33 +0300

    First version of the game collection view

--- gamez/views.py
+++ gamez/views.py
@@ -1,12 +1,21 @@
+from site import PREFIXES
+from urllib.parse import quote_from_bytes
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import models
 from django.contrib.auth.decorators import login_required
+from .models import Collection, Game, Platform
 
 
 # The main page that lets users manage their game collection
 @login_required
 def index(request):
-    return render(request, 'gamez/index.html')
+    
+    uid = request.user.id
+    collection = Collection.objects.filter(user_id=uid).first()
+    games = Game.objects.filter(collection_id=collection.id).select_related('platform')
+
+    return render(request, 'gamez/index.html', {'collection': collection, 'games': games})
 
 
 # The no-login brag page that a user can share with other people
