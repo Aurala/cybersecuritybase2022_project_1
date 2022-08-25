c63106c71b3ca30652aa082e2967b8b50aad3750
Aurala <39862070+Aurala@users.noreply.github.com>
2022-08-25 19:27:28 +0300

    Fixing login issues

--- cybersecuritybase2022_project_1/settings.py
+++ cybersecuritybase2022_project_1/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "gamez/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 FIXTURE_DIRS = [BASE_DIR / "fixtures"]
