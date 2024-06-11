35a2b73339afb042d984df41c3b93e59bac85596
Markus Aurala <markus.aurala@gmail.com>
2024-06-11 18:43:39 +0300

    Update settings.py

--- cybersecuritybase2022_project_1/settings.py
+++ cybersecuritybase2022_project_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^p*$i5-318)*p&%=4)_d$cv4klyt5^1e_(p@oly^2pwl(v!8&5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
